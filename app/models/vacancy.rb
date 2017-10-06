2f8a8b4984e715aeb980b0778fa34774af3a4475
Simao Belchior <simaobelchior@gmail.com>
2017-10-06 11:09:50 +0100

    Updates syntax fixing deprecation warning

--- app/models/vacancy.rb
+++ app/models/vacancy.rb
@@ -1,3 +1,4 @@
 class Vacancy < ApplicationRecord
-  validates :url, format: { with: /\A#{URI.regexp(['http', 'https']).to_s}\z/ }, if: 'url.present?'
+  validates :url, format: { with: /\A#{URI.regexp(['http', 'https']).to_s}\z/ },
+    if: lambda { |vacancy| vacancy.url.present? }
 end
